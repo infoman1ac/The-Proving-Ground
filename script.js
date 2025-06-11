1d07d2c7655bf2763fe9d363ce63ae9b1a061a21
infoman1ac <notinfomaniac@gmail.com>
2025-06-11 21:30:17 +0100

    update quotes generation

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const authorEl = document.getElementById("quote-author");
 const quoteEl = document.getElementById("quote-text");
 const genBtn = document.getElementById("gen-btn");
-const url = "https://fakestoreapi.com/products/1";
+const url = "https://dummyjson.com/quotes";
+let quotes = [];
 
-fetch(url)
-    .then((response) => {
+fetch("https://dummyjson.com/quotes")
+    .then(response => {
         if (!response.ok) {
-            throw new Error("Network response was not okay");
+            throw new Error('Network response was not okay');
         }
         return response.json();
     })
-    .then((data) => console.log(data.description));
+    .then(data => {
+        quotes = data.quotes;
+        console.log(quotes)
+        showRandomQuote();
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 
-.catch ((error) =>
-    console.error("Error:", error)
-);
+function showRandomQuote() {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    quoteEl.textContent = randomQuote.quote;
+    authorEl.textContent = randomQuote.author;
+}
+
+genBtn.addEventListener("click", showRandomQuote);
